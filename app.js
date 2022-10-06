f5c3da8e9254653d10ed18481cdfa4c96b259671
Lautaro Parandelli <lautaroparandelli@gmail.com>
2022-10-05 21:15:50 -0300

    variable de entorno heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, '/public')));
-app.listen(3000, function () { console.log("Funcionando en el puerto 3000")})
+app.listen(process.env.PORT || 3000, function () { console.log("Funcionando en el puerto 3000")})
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/views/home.html'))})
 app.get('/register', function (req, res) {
