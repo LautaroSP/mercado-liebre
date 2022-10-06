b4fb254795f33dd1d83d15cb0151f6e5228e3441
Lautaro Parandelli <lautaroparandelli@gmail.com>
2022-10-05 21:13:28 -0300

    subo el proyecto

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.listen(3000, function () { console.log("Funcionando en el puerto 3000")})
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/home.html'))})
+app.get('/register', function (req, res) {
+        res.sendFile(path.join(__dirname, '/views/register.html'))})
+app.get('/login', function (req, res) {
+            res.sendFile(path.join(__dirname, '/views/login.html'))})
